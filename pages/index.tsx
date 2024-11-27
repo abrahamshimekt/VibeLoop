b7bda42932a5232a4d2e0adea859754bb6327ad4
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-11-27 19:57:48 +0300

    display videos

---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { Video } from "../types";
+import NoResults from "@/components/NoResults";
+import VideoCard from "@/components/VideoCard";
+import { BASE_URL } from "@/utils";
+import { MdOutlineVideocamOff } from "react-icons/md";
+interface IProps {
+  videos: Video[];
+}
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className="flex flex-col  gap-10 vidoes h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text="No Videos" icon={MdOutlineVideocamOff} />
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  try {
+    let response = null;
+    if (topic) {
+      response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+    } else {
+      response = await axios.get(`${BASE_URL}/api/post`);
+    }
+    return { props: { videos: response?.data } };
+  } catch (error) {
+    return { props: { videos: [] } };
+  }
+};
+
+export default Home;
