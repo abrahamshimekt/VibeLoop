c63d00d0eabdaf0ac70870b1678ffca4fc50e793
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-11-27 19:58:11 +0300

    configure

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,44 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      width: {
+        1600: '1600px',
+        400: '400px',
+        450: '450px',
+        210: '210px',
+        550: '550px',
+        260: '260px',
+        650: '650px',
+      },
+      height: {
+        600: '600px',
+        280: '280px',
+        900: '900px',
+        458: '458px',
+      },
+      top: {
+        ' 50%': '50%',
+      },
+      backgroundColor: {
+        primary: '#F1F1F2',
+        blur: '#030303',
+      },
+      colors: {
+        primary: '#0097b2',
+      },
+
+      backgroundImage: {
+        'blurred-img':
+          "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsaaJ7s4lqcBF4IDROVPzrlL5fexcwRmDlnuEYQenWTt1DejFY5kmYDref2a0Hp2eE4aw&usqp=CAU')",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
