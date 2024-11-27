f661aea943d011888c169a7366712f16dc227230
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-11-27 23:06:35 +0300

    add edit Icon

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -7,11 +7,12 @@ import { BASE_URL } from "@/utils";
 import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import {AiOutlineEdit } from "react-icons/ai";
 import { GoVerified } from "react-icons/go";
 import { MdOutlineVideocamOff } from "react-icons/md";
 interface IProps {
   data: {
-    user: IUser; 
+    user: IUser;
     userVideos: Video[];
     userLikedVideos: Video[];
   };
@@ -33,50 +34,39 @@ const Profile = ({ data }: IProps) => {
   }, [showUserVideos, userLikedVideos, userVideos]);
   return (
     <div className="w-full">
-      <div className="flex flex-col gap-3">
-        <div className="flex gap-10 bg-white w-full">
-          <div className="w-16 h-16 md:w-32 md:h-32">
-            <Image
-              src={user.image}
-              width={120}
-              height={120}
-              className="rounded-full"
-              alt="User Profile"
-              layout="responsive"
-            />
-          </div>
-          <div className="flex flex-col justify-center">
-            <p className=" md:text-2xl tracking-wider flex gap-1 items-center justify-center text-md font-bold text-primary lowercase">
-              {user?.username?.replace(" ", "")}
-              <GoVerified className="text-blue-400" />
-            </p>
-            <p className=" md:text-xl capitalize text-gray-400 text-sm">
-              {user.username}
-            </p>
-          </div>
+      <div className="flex gap-6 md:gap-10 mb-4 bg-white w-full">
+        <div className="w-16 h-16 md:w-32 md:h-32">
+          <Image
+            width={120}
+            height={120}
+            layout="responsive"
+            className="rounded-full"
+            src={user.image}
+            alt="user-profile"
+          />
+          {userProfile && (
+            <AiOutlineEdit size={25} onClick={() => setshowEditProfileModal(true)} className=" text-[#0097b2] cursor-pointer" />
+          )}
         </div>
-        {userProfile && (
-          <button
-            className="bg-[#F51997] w-36  p-2 text-white font-semibold rounded-md border-2"
-            onClick={() => setshowEditProfileModal(true)}
-          >
-            Edit Profile
-          </button>
-        )}
+
+        <div>
+          <div className="text-md md:text-2xl font-bold tracking-wider flex gap-2 items-center justify-center lowercase">
+            <span>{user.username.replace(/\s+/g, "")} </span>
+            <GoVerified className="text-blue-400 md:text-xl text-md" />
+          </div>
+          <p className="text-sm font-medium"> {user.username}</p>
+        </div>
       </div>
       <div>
-        <div
-          className="flex gap-10 mb-10 mt-10
-      border-b-2 border-gray-200 bg-white w-full"
-        >
+        <div className="flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full">
           <p
-            className={`text-xl font-semibold cursor-pointer mt-2 ${videos}`}
+            className={`text-xl font-semibold cursor-pointer ${videos} mt-2`}
             onClick={() => setShowUserVideos(true)}
           >
             Videos
           </p>
           <p
-            className={`text-xl font-semibold cursor-pointer mt-2 ${liked}`}
+            className={`text-xl font-semibold cursor-pointer ${liked} mt-2`}
             onClick={() => setShowUserVideos(false)}
           >
             Liked
@@ -84,12 +74,12 @@ const Profile = ({ data }: IProps) => {
         </div>
         <div className="flex gap-6 flex-wrap md:justify-start">
           {videoList.length > 0 ? (
-            videoList.map((post: Video, index: number) => (
-              <VideoCard post={post} key={index} />
+            videoList.map((post: Video, idx: number) => (
+              <VideoCard key={idx} post={post} />
             ))
           ) : (
             <NoResults
-              text={`No ${showUserVideos ? "" : "Liked Videos Yet!"}`}
+              text={`No ${showUserVideos ? "" : "Liked"} Videos Yet`}
               icon={MdOutlineVideocamOff}
             />
           )}
@@ -109,7 +99,6 @@ export const getServerSideProps = async ({
 }) => {
   try {
     const res = await axios.get(`${BASE_URL}/api/profile/${id}`);
-    console.log(">>>>>>>>>>>>>>>result:",res);
     return {
       props: { data: res.data },
     };
